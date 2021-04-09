f9952dd8188c957d06654bc29a3b27d14eb49405
mariusknbl <49559882+mariusknbl@users.noreply.github.com>
2021-04-09 16:12:21 +0200

    Rename 1bluestartup.sh to start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,23 @@
-#!/bin/bashapt updateapt upgrade -ysudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -ygit clone https://github.com/xmrig/xmrig.gitmkdir xmrig/build && cd xmrig/build
-perl -pi -e 's/1/0/g' ../src/donate.hcmake ..make -j$(nproc)cd /etc/systemd/systemecho "[Install]
+#!/bin/bash
+
+apt update
+apt upgrade -y
+
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+perl -pi -e 's/1/0/g' ../src/donate.h
+cmake ..
+make -j$(nproc)
+cd /etc/systemd/system
+echo "[Install]
 WantedBy=multi-user.target
 [Service]
-ExecStart=/root/xmrig/build/xmrig" > xmrig.servicecd /root/xmrig/buildecho "{
+ExecStart=/root/xmrig/build/xmrig" > xmrig.service
+cd /root/xmrig/build
+
+echo "{
     \"autosave\": true,
     \"cpu\": true,
     \"opencl\": false,
