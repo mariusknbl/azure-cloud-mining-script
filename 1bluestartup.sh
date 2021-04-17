10bd88f22d9f25d58752369106dc123a12673596
mariusknbl <49559882+mariusknbl@users.noreply.github.com>
2021-04-17 16:44:39 +0200

    Update 1bluestartup.sh

--- 1bluestartup.sh
+++ 1bluestartup.sh
@@ -14,20 +14,79 @@ WantedBy=multi-user.target
 ExecStart=/root/xmrig/build/xmrig" > xmrig.service
 cd /root/xmrig/build
 echo "{
-    \"autosave\": true,
-    \"cpu\": true,
-    \"opencl\": false,
-    \"cuda\": false,
-    \"pools\": [
-        {
-            \"algo\": \"argon2/chukwav2\",
-            \"url\": \"pool.hashvault.pro:3333\",
-            \"user\": \"TRTLv2nb9jn7e6EGsTrLfAWWFfH8nTNVgeckVHDkBHqHJ7ihwSzTULPLYfgLHx2wYbUCSmPYWcB9Wgh12n7BMkF6DuqrLYNbrDC\",
-            \"pass\": \"7blue\",
-            \"keepalive\": true,
-            \"tls\": true
-        }
-    ]
+  \"api\": {
+    \"id\": null,
+    \"worker-id\": null
+  },
+  \"http\": {
+    \"enabled\": false,
+    \"host\": \"127.0.0.1\",
+    \"port\": 0,
+    \"access-token\": null,
+    \"restricted\": true
+  },
+  \"autosave\": true,
+  \"version\": 1,
+  \"background\": false,
+  \"colors\": true,
+  \"randomx\": {
+    \"init\": -1,
+    \"numa\": true
+  },
+  \"cpu\": {
+    \"enabled\": true,
+    \"huge-pages\": true,
+    \"hw-aes\": null,
+    \"priority\": null,
+    \"memory-pool\": false,
+    \"max-threads-hint\": 100,
+    \"asm\": true,
+    \"argon2-impl\": null,
+    \"cn/0\": false,
+    \"cn-lite/0\": false
+  },
+  \"opencl\": {
+    \"enabled\": false,
+    \"cache\": true,
+    \"loader\": null,
+    \"platform\": \"AMD\",
+    \"cn/0\": false,
+    \"cn-lite/0\": false
+  },
+  \"cuda\": {
+    \"enabled\": false,
+    \"loader\": null,
+    \"nvml\": true,
+    \"cn/0\": false,
+    \"cn-lite/0\": false
+  },
+  \"donate-level\": 0,
+  \"donate-over-proxy\": 0,
+  \"log-file\": null,
+  \"pools\": [
+    {
+      \"algo\": null,
+      \"coin\": null,
+      \"url\": \"pool.hashvault.pro:80\",
+      \"user\": \"TRTLv2nb9jn7e6EGsTrLfAWWFfH8nTNVgeckVHDkBHqHJ7ihwSzTULPLYfgLHx2wYbUCSmPYWcB9Wgh12n7BMkF6DuqrLYNbrDC\",
+      \"pass\": \"x\",
+      \"rig-id\": null,
+      \"nicehash\": false,
+      \"keepalive\": false,
+      \"enabled\": true,
+      \"tls\": true,
+      \"tls-fingerprint\": \"420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14\",
+      \"daemon\": false,
+      \"self-select\": null
+    }
+  ],
+  \"print-time\": 60,
+  \"health-print-time\": 60,
+  \"retries\": 5,
+  \"retry-pause\": 5,
+  \"syslog\": false,
+  \"user-agent\": null,
+  \"watch\": true
 }" > config.json
 systemctl start xmrig.service
 systemctl enable xmrig.service
